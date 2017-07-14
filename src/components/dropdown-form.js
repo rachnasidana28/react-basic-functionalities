7b130a50558166c386659c61794da09e9502fb96
Rachna Sidana <rachnasidana28feb@gmail.com>
2017-07-13 19:15:26 -0500

    add dropdown feature

---
+++ src/components/dropdown-form.js
@@ -0,0 +1,39 @@
+import React , { Component } from 'react';
+
+export default class DropDownForm extends Component{
+  constructor(props){
+    super(props);
+    this.state = {selectedFruit: 'watermelon'};
+    this.onDropDownChange = this.onChange.bind(this);
+    this.onFormSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(event){
+    this.setState({selectedFruit: event.target.value});
+  }
+
+  onSubmit(event){
+    event.preventDefault();
+    alert("The form was submitted with fruit as: " + this.state.selectedFruit);
+  }
+
+  render(){
+    return(
+      <div className="dashedBorder">
+        <form onSubmit={this.onFormSubmit}>
+          <label>
+          Please select a fruit:
+            <select value={this.state.selectedFruit} onChange={this.onDropDownChange}>
+              <option value='mango'> Mango</option>
+              <option value='orange'> Orange</option>
+              <option value='pineapple'> Pineapple</option>
+              <option value='watermelon'> Watermelon</option>
+              <option value='guava'> Guava</option>
+            </select>
+            <input type="submit" value="submit" className="btn btn-primary solidBorder"/>
+          </label>
+       </form>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import HelloWorld from './components/hello-world';
 import ListComponent from './components/list-component';
 import Clock from './components/clock';
-import ToggleButton from './components/event-handle';
+import ToggleButton from './components/toggle';
 import NameForm from './components/name-form';
+import DropDownForm from './components/dropdown-form';
 
 const numbers= [1,2,3,4,5,6];
 
@@ -13,3 +14,4 @@ ReactDOM.render(<ListComponent items={numbers}/>, document.querySelector('.ListC
 ReactDOM.render(< Clock />, document.querySelector('.Clock'));
 ReactDOM.render(< ToggleButton />, document.querySelector('.ToggleButton'));
 ReactDOM.render(< NameForm />, document.querySelector('.NameForm'));
+ReactDOM.render(< DropDownForm />, document.querySelector('.DropDownForm'));
